3373c7595db1998b44abc9518d28dbd0eabbdc45
Lwam Tekie <ltekie87@yahoo.com>
2019-03-05 20:26:52 -0600

    finished dinosaurs

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const dinosaurs = [
     {
         dinoType: 't-rex',
+        name: 'wrex'
     },
     {
         dinoType: 'stegosaurus',
+        name: 'steve'
     },
     {
         dinoType: 'velociraptor',
+        name: 'Ted',
     }
 ];
 
@@ -17,12 +20,15 @@ const dinosaurs = [
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
-}
+};
 
 const buildDinosaurs = () => {
     let domstring = '';
     for (let i = 0; i < dinosaurs.length; i++) {
+        domstring += `<div class="dinosaur">`
         domstring += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        domstring += `<p>${dinosaurs[i].name}</p>`
+        domstring += `</div>`
     }
     printToDom('dino-barn', domstring)
 };
